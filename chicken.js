a56fb6fcec2035c7aa197b953a0ddf05a56eab3a
Ogbevire O., Obogrow <ogbevire@gmail.com>
2023-10-20 11:15:54 +0100

    manipulating attributes

---
+++ chicken.js
@@ -0,0 +1,6 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+
+
+const img = document.querySelector('img');
+img.setAttribute('src', 'https://devsprouthosting.com/images/chicken.jpg')
+img.setAttribute('alt', 'chicken')
