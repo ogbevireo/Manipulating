1fe657b778e31e9c8ecd555bcd29efdf3c100684
Ogbevire O., Obogrow <ogbevire@gmail.com>
2023-10-21 08:14:25 +0100

    styling with Javascript

---
+++ magicalForest.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('container');
+container.style.textAlign = 'center'
+// container.style.color = 'blue';
+const image = document.querySelector('img');
+image.style.width = '150px';
+image.style.borderRadius = '50%';
